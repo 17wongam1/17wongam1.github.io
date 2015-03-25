cd13a4684cc09b1b98f2fbb22aee7143dbcc746c
The2560x1440 <17wongam1@stconline.edu.hk>
2015-03-25 14:33:00 +0800

    change

--- doodle/main.js
+++ doodle/main.js
@@ -20,7 +20,7 @@ var platforms = [],
   image = document.getElementById("sprite"),
   player, platformCount = 10,
   position = 0,
-  gravity = 0.2,
+  gravity = 0.0,
   animloop,
   flag = 0,
   menuloop, broken = 0,
@@ -139,11 +139,11 @@ function Platform() {
   //3: Breakable (Go through)
   //4: Vanishable 
   //Setting the probability of which type of platforms should be shown at what score
-  if (score >= 5000) this.types = [2, 3, 3, 3, 4, 4, 4, 4];
-  else if (score >= 2000 && score < 5000) this.types = [2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4];
-  else if (score >= 1000 && score < 2000) this.types = [2, 2, 2, 3, 3, 3, 3, 3];
-  else if (score >= 500 && score < 1000) this.types = [1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3];
-  else if (score >= 100 && score < 500) this.types = [1, 1, 1, 1, 2, 2];
+  if (score >= 5000) this.types = [1,3];
+  else if (score >= 2000 && score < 5000) this.types = [1,3];
+  else if (score >= 1000 && score < 2000) this.types = [1,3];
+  else if (score >= 500 && score < 1000) this.types = [1,3];
+  else if (score >= 100 && score < 500) this.types = [1,3];
   else this.types = [1];
 
   this.type = this.types[Math.floor(Math.random() * this.types.length)];
@@ -151,10 +151,11 @@ function Platform() {
   //We can't have two consecutive breakable platforms otherwise it will be impossible to reach another platform sometimes!
   if (this.type == 3 && broken < 1) {
     broken++;
-  } else if (this.type == 3 && broken >= 1) {
-    this.type = 1;
-    broken = 0;
-  }
+  } 
+  // else if (this.type == 3 && broken >= 1) {
+  //   this.type = 1;
+  //   broken = 0;
+  
 
   this.moved = 0;
   this.vx = 1;
